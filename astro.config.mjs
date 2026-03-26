38cbd9eec995a7be7b938aa4c3524a5f316ab9de
Riley <rmcaldwell2015@gmail.com>
2026-03-26 10:32:23 -0400

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://www.docksideweb.com",
-  trailingSlash: 'ignore',
+  trailingSlash: 'always',
   output: "static",
   vite: {
     plugins: [tailwindcss()],
